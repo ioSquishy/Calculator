02efedbd854351f0eab62a0b51c7865aa0e15edd
ioSquishy <squishylxix@gmail.com>
2026-01-29 11:46:00 -0800

    added click event listener and event handling for calculator

---
+++ calculator.js
@@ -0,0 +1,36 @@
+const calculator = document.querySelector("#calculator");
+const screen = document.querySelector("#screen");
+const buttons = document.querySelectorAll(".buttons button");
+
+// Listeners
+//Click Listener
+buttons.forEach(button => {
+  button.addEventListener("click", onClickListener);
+})
+function onClickListener(event) {
+  let value = event.target.innerText;
+  console.log(`Clicked: ${value}`);
+
+  if (value === "C") {
+    input = "";
+  } else if (value === "=") {
+    input = calculate(input);
+  } else if (isNaN(value)) {
+    input += ` ${value} `;
+  } else {
+    input += `${value}`;
+  }
+
+  screen.innerText = input;
+}
+
+// Calculator logic
+let input = "";
+/**
+ * Calculates result from plain-text equation
+ * @param {String} equation 
+ * @return {Number} result
+ */
+function calculate(equation) {
+  console.log(`Calculating: ${equation}`);
+}
