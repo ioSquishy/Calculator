7ddba73272808e0e6e9529d7cbef0b0b4e407903
ioSquishy <squishylxix@gmail.com>
2026-02-06 12:08:11 -0800

    updated input logic to use an array

--- calculator.js
+++ calculator.js
@@ -7,90 +7,60 @@ const buttons = document.querySelectorAll(".buttons button");
 buttons.forEach(button => {
   button.addEventListener("click", onClickListener);
 })
-let input = "";
-let lastNum = 0;
-let lastNumStartIndex = 0;
+let input = [0];
 let inputIsResult = false;
 function onClickListener(event) {
   let value = event.target.innerText;
   console.log(`Clicked: ${value}`);
-
-  /**
-   * todo: bug fix
-   * for some reason after press =,
-   * the lastNumStartIndex is 1 greater than it should
-   */
+  
   if (value === "C") {
-    input = "";
-    lastNum = 0;
-    lastNumStartIndex = 0;
+    input = [0];
   } else if (value === "=") {
-    let result = calculate(input);
-    input = result.toLocaleString('en-US');
-    screen.innerText += ` = \n${input}`;
-    lastNum = result;
-    inputIsResult = true;
-    lastNumStartIndex = 0;
+    let numberResult = calculate(input);
+    let localeResult = numberResult.toLocaleString();
+    screen.innerText += ` = \n${localeResult}`;
+    input = [numberResult];
     return;
   } else if (isNaN(value)) {
-    inputIsResult = false;
-    input += ` ${value} `;
-    lastNumStartIndex = input.length + 1;
-    lastNum = 0;
+    input.push(value);
+    input.push(0);
   } else {
-    if (inputIsResult) {
-      lastNum = 0;
-      inputIsResult = false;
+    if (input.length === 1 && input.at(-1) != 0) {
+      input = [0];
     }
-    lastNum = (lastNum * 10) + Number(value);
-    input = input.substring(0, lastNumStartIndex) + lastNum.toLocaleString('en-US');
+    input[input.length - 1] = (input.at(-1) * 10) + Number(value);
   }
 
-  screen.innerText = input;
+  renderInputOnScreen();
+}
+
+function renderInputOnScreen() {
+  let output = "";
+
+  for (let item of input) {
+    if (isNaN(item)) {
+      output += ` ${item} `;
+    } else if (item != 0) {
+      output += item.toLocaleString();
+    }
+  }
+
+  screen.innerText = output;
 }
 
 // Calculator logic
 /**
- * Calculates result from plain-text equation
- * @param {String} equation 
+ * Calculates result from array of inputs
+ * @param {Array} equation 
  * @return {Number} result
  */
 function calculate(equation) {
   console.log(`Calculating: ${equation}`);
-  // clean up equation
-  let cleanEquation = "";
-  for (let i = 0; i < equation.length; i++) {
-    // skip/replace cosmetic characters
-    switch (equation[i]) {
-      case "×":
-        cleanEquation += "*";
-      case "*":
-      case " ":
-      case ",":
-        continue;
-      default:
-        // if not a number, ensure character is expected
-        if (isNaN(equation[i])) {
-          switch(equation[i]) {
-            case "+":
-            case "-":
-            case "/":
-              cleanEquation += String(equation[i]);
-              continue;
-            default:
-              console.log(`Unexpected value in input: ${equation[i]}`);
-              continue;
-          }
-        } else {
-          // if a number, add it to equation
-          cleanEquation += String(equation[i]);
-        }
-    }
-  }
-  console.log(`Cleaned input: ${cleanEquation}`);
+  let result = 1;
+
+  //TODO calculatee
 
   // evaulate equation
-  let result = eval(cleanEquation);
   if (!isFinite(result)) {
     result = "undefined"
   }
